1538ddb60c3860d26d5b5fad1becaaf8d7d951cd
Kevin Khov <khovkevin25@gmail.com>
2025-01-06 19:39:54 -0500

    Crowdfunding Database

---
+++ Postgres Database/crowdfunding_db_select.sql
@@ -0,0 +1,11 @@
+-- Select statement to pull all values from category table
+SELECT * FROM category;
+
+-- Select statement to pull all values from subategory table
+SELECT * FROM subcategory;
+
+-- Select statement to pull all values from campaign table
+SELECT * FROM campaign;
+
+-- Select statement to pull all values from contacts table
+SELECT * FROM contacts;
